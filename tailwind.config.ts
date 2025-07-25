1ded934891c2ea8dd98ed8327e800260e4105b6a
Adwitya Hersa <adwihersa@gmail.com>
2025-07-25 16:31:04 +0700

    Finalize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,6 +29,9 @@ const config: Config = {
           "850": "#FDFDFD",
           "900": "#FFFFFF",
         },
+        link: {
+          "100": "#1DA1F2",
+        },
       },
       boxShadow: {
         "light-100":
@@ -46,12 +49,18 @@ const config: Config = {
         "space-grotesk": ["var(--font-space-grotesk)"],
       },
       borderRadius: {
+        "2": "8px",
+        "1.5": "6px",
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      backgroundImage: {
+        "auth-dark": 'url("/images/auth-dark.png")',
+        "auth-light": 'url("/images/auth-light.png")',
+      },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 export default config;
